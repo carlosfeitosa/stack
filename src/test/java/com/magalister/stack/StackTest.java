f69678abfd6fdbd316b14162ee2f32c879015be9
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-01-21 11:51:33 -0300

    Commit inicial

---
+++ src/test/java/com/magalister/stack/StackTest.java
@@ -0,0 +1,68 @@
+package com.magalister.stack;
+
+import org.junit.jupiter.api.Test;
+
+public class StackTest {
+	@Test
+	/**
+	 * Testa se a pilha consegue receber um "push", adicionando o elemento ao topo
+	 * da pilha.
+	 */
+	public void consegueAdicionarItem() {
+
+	}
+
+	/**
+	 * Testa se a pilha consegue receber um "pop", recuperando e removendo o
+	 * elemento do tipo da pilha.
+	 */
+	@Test
+	public void conseguePegarItem() {
+
+	}
+
+	@Test
+	public void conseguePegarItemSemRemover() {
+
+	}
+
+	@Test
+	public void consegueSaberSeEstaVazio() {
+
+	}
+
+	@Test
+	public void consegueSaberSeNaoEstaVazio() {
+
+	}
+
+	@Test
+	public void consegueSaberSeEstaCheio() {
+
+	}
+
+	@Test
+	public void consegueSaberSeNaoEstaCheio() {
+
+	}
+
+	@Test
+	public void consegueDeterminarSeuTamanho() {
+
+	}
+
+	@Test
+	public void consegueLimpar() {
+
+	}
+
+	@Test
+	public void consegueEmpilhar() {
+
+	}
+
+	@Test
+	public void consegueEstourar() {
+
+	}
+}
