8dda0980e10d4bd8a0561568d360a88c6240978b
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-01-21 20:50:20 -0300

    Escrita dos testes

--- src/test/java/com/magalister/pilha/PilhaTest.java
+++ src/test/java/com/magalister/pilha/PilhaTest.java
@@ -1,10 +1,23 @@
 package com.magalister.pilha;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PilhaTest {
-	
+
+	private Pilha pilha;
+
+	private static final int TAMANHO = 10;
+	private static final int TAMANHO_REDUZIDO = 2;
+
+	private static final String ASSERT_MSG_TAMANHO_FILA = "Tamanho da pilha diferente da esperada";
+	private static final String ASSERT_MSG_NUMERO = "Número diferente do esperado";
+	private static final String ASSERT_MSG_EXCECAO = "Exceção diferente da esperada";
+
 	/**
 	 * Inicializa a pilha.
 	 * 
@@ -12,6 +25,7 @@ class PilhaTest {
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		pilha = new Pilha(TAMANHO);
 	}
 
 	/**
@@ -20,7 +34,12 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueAdicionarItem() {
+		int numero = 7777;
+		int tamanhoPilha = pilha.size();
 
+		pilha.push(numero);
+
+		assertTrue(pilha.size() == (tamanhoPilha + 1), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -29,7 +48,15 @@ class PilhaTest {
 	 */
 	@Test
 	public void conseguePegarItem() {
+		int numero = 7777;
+		int tamanhoPilha = pilha.size();
 
+		pilha.push(numero);
+
+		int numeroPop = pilha.pop();
+
+		assertEquals(numero, numeroPop, ASSERT_MSG_NUMERO);
+		assertTrue(pilha.size() == (tamanhoPilha - 1), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -38,7 +65,15 @@ class PilhaTest {
 	 */
 	@Test
 	public void conseguePegarItemSemRemover() {
+		int numero = 7777;
+		int tamanhoPilha = pilha.size();
 
+		pilha.push(numero);
+
+		int numeroPop = pilha.pop();
+
+		assertEquals(numero, numeroPop, ASSERT_MSG_NUMERO);
+		assertTrue(pilha.size() == tamanhoPilha, ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -46,7 +81,7 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueSaberSeEstaVazio() {
-
+		assertTrue(pilha.isEmpty(), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -54,7 +89,11 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueSaberSeNaoEstaVazio() {
+		int numero = 7777;
 
+		pilha.push(numero);
+
+		assertTrue(!pilha.isEmpty(), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -62,7 +101,14 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueSaberSeEstaCheio() {
+		pilha = new Pilha(TAMANHO_REDUZIDO);
 
+		int numero = 7777;
+
+		pilha.push(numero);
+		pilha.push(numero);
+
+		assertTrue(pilha.isFull, ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -70,7 +116,13 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueSaberSeNaoEstaCheio() {
+		pilha = new Pilha(TAMANHO_REDUZIDO);
 
+		int numero = 7777;
+
+		pilha.push(numero);
+
+		assertTrue(!pilha.isFull, ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -78,7 +130,14 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueDeterminarSeuTamanho() {
+		int numero = 7777;
+		int quantidade = 9;
 
+		for (int i = 0; i < quantidade; i++) {
+			pilha.push(numero);
+		}
+
+		assertEquals(quantidade, pilha.size(), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -86,15 +145,13 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueLimpar() {
+		int numero = 7777;
 
-	}
+		pilha.push(numero);
 
-	/**
-	 * Testa se a pilha está armazenando os itens na ordem correta.
-	 */
-	@Test
-	public void consegueEmpilhar() {
+		pilha.clear();
 
+		assertEquals(0, pilha.size(), ASSERT_MSG_TAMANHO_FILA);
 	}
 
 	/**
@@ -102,7 +159,34 @@ class PilhaTest {
 	 */
 	@Test
 	public void consegueEstourar() {
+		int numero = 7777;
+		int quantidade = TAMANHO + 1;
 
+		assertThrows(EstouroDePilhaException.class, () -> {
+			for (int i = 0; i < quantidade; i++) {
+				pilha.push(numero);
+			}
+		}, ASSERT_MSG_EXCECAO);
+	}
+
+	/**
+	 * Testa se a pilha está armazenando os itens na ordem correta.
+	 */
+	@Test
+	public void consegueEmpilhar() {
+		int[] itens = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] itensReverso = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		for (int i = 0; i < itens.length; i++) {
+			pilha.push(itens[i]);
+		}
+
+		for (int i = 0; i < itensReverso.length; i++) {
+			int numeroExperado = itensReverso[i];
+			int numeroPilha = pilha.pop();
+
+			assertEquals(numeroExperado, numeroPilha, ASSERT_MSG_NUMERO);
+		}
 	}
 
 }
