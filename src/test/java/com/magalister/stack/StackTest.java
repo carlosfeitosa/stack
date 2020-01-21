f7db4405dfbca2d4837873acffc6102dffe09f4e
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-01-21 18:12:04 -0300

    esqueleto do caso de testes

--- src/test/java/com/magalister/stack/StackTest.java
+++ src/test/java/com/magalister/stack/StackTest.java
@@ -1,6 +1,5 @@
 package com.magalister.stack;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,71 +15,91 @@ class StackTest {
 	}
 
 	/**
-	 * Limpa a pilha.
-	 * 
-	 * @throws Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	/**
 	 * Testa se a pilha consegue adicionar um item (push), no topo da estrutura.
+	 * Pode estourar uma exceção por estouro de pilha.
 	 */
+	@Test
 	public void consegueAdicionarItem() {
 
 	}
 
 	/**
-	 * Testa se a pilha consegue receber um "pop", recuperando e removendo o
-	 * elemento do tipo da pilha.
+	 * Testa se a pilha consegue recuperar e remover (pop) um item, no topo da
+	 * estrutura. Pode estourar uma exceção por não haver elementos.
 	 */
 	@Test
 	public void conseguePegarItem() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue recuperar sem remover (peek) um item, no topo da
+	 * estrutura. Pode estourar exceção por não haver elementos.
+	 */
 	@Test
 	public void conseguePegarItemSemRemover() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue saber se está vazia.
+	 */
 	@Test
 	public void consegueSaberSeEstaVazio() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue saber se não está vazia.
+	 */
 	@Test
 	public void consegueSaberSeNaoEstaVazio() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue saber se está cheia.
+	 */
 	@Test
 	public void consegueSaberSeEstaCheio() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue saber se não está cheia.
+	 */
 	@Test
 	public void consegueSaberSeNaoEstaCheio() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue determinar seu tamanho.
+	 */
 	@Test
 	public void consegueDeterminarSeuTamanho() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue se limpar.
+	 */
 	@Test
 	public void consegueLimpar() {
 
 	}
 
+	/**
+	 * Testa se a pilha está armazenando os itens na ordem correta.
+	 */
 	@Test
 	public void consegueEmpilhar() {
 
 	}
 
+	/**
+	 * Testa se a pilha consegue estourar.
+	 */
 	@Test
 	public void consegueEstourar() {
 
